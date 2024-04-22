b9356260f3c45edd2758dda90bb072fe1befbdae
Marcel Herr <mrclhrr@gmail.com>
2024-04-22 12:16:42 +0200

    implement feature create entry

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -7,11 +7,22 @@ export default async function handler(request, response) {
 
     if (request.method === "GET") {
       const places = await Places.find();
-      console.log(places);
       return response.status(200).json(places);
     }
   } catch (error) {
     console.error("Error in Fetching;", error);
     return response.status(500).json({ error: "Internal Server Error" });
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Places.create(placeData);
+
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
 }
